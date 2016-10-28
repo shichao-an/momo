fc1a0ec9d6ae1db2f91e9eed7ace40c174bdcdc1
Shichao An <shichao.an@nyu.edu>
2016-10-28 12:07:57 -0700

    Use ruamel.yaml as yaml backend

--- momo/backends/yaml.py
+++ momo/backends/yaml.py
@@ -1,33 +1,6 @@
 from __future__ import absolute_import
 from momo.backends.base import Document
-import yaml
-from collections import OrderedDict
-
-
-# http://stackoverflow.com/a/21912744/1751342
-def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
-    class OrderedLoader(Loader):
-        pass
-
-    def construct_mapping(loader, node):
-        loader.flatten_mapping(node)
-        return object_pairs_hook(loader.construct_pairs(node))
-    OrderedLoader.add_constructor(
-        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        construct_mapping)
-    return yaml.load(stream, OrderedLoader)
-
-
-def ordered_dump(data, stream=None, Dumper=yaml.Dumper, **kwargs):
-    class OrderedDumper(Dumper):
-        pass
-
-    def _dict_representer(dumper, data):
-        return dumper.represent_mapping(
-            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-            data.items())
-    OrderedDumper.add_representer(OrderedDict, _dict_representer)
-    return yaml.dump(data, stream, OrderedDumper, **kwargs)
+import ruamel.yaml
 
 
 class BucketDocument(Document):
@@ -50,12 +23,12 @@ class BucketDocument(Document):
 
         """
         with open(self.path) as f:
-            return ordered_load(f.read())
+            return ruamel.yaml.load(f.read(), ruamel.yaml.RoundTripLoader)
 
     def dump(self, content):
         """
         Dump the content to the bucket file.
         """
         with open(self.path, 'w') as f:
-            ordered_dump(content, f, default_flow_style=False,
-                         allow_unicode=True)
+            ruamel.yaml.round_trip_dump(
+                content, f, indent=4, block_seq_indent=2, width=500)
