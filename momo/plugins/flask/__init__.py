7829a95c86ec7cff8e2426119f512b422b3bf803
Shichao An <shichao.an@nyu.edu>
2017-02-11 23:56:08 -0800

    Pass in configs and add ChoiceLoader to reset template loader

--- momo/plugins/flask/__init__.py
+++ momo/plugins/flask/__init__.py
@@ -1,5 +1,11 @@
+import os
+import jinja2
 from momo.plugins.base import Plugin
 from momo.plugins.flask.app import app
+
+FLASK_DEFAULT_HOST = '127.0.0.1'
+FLASK_DEFAULT_PORT = '7000'
+FLASK_DEFAULT_DEBUG = True
 
 
 class Flask(Plugin):
@@ -7,11 +13,32 @@ class Flask(Plugin):
         bucket_name = self.settings.bucket.name
         self.configs = self.settings.plugins.get(
             'flask', {}).get(bucket_name, {})
+        flask_root_dir = os.path.join(
+            self.settings.settings_dir, 'flask')
+        self.flask_dir = os.path.join(flask_root_dir, bucket_name)
+        self.template_dir = os.path.join(self.flask_dir, 'templates')
+        self.static_dir = os.path.join(self.flask_dir, 'static')
+        self._reset_loader()
+
+    def _reset_loader(self):
+        """Add user-defined template folder."""
+        app.jinja_loader = jinja2.ChoiceLoader([
+            jinja2.FileSystemLoader(self.template_dir),
+            app.jinja_loader,
+        ])
 
     def run(self, args=None):
+        # args is not used for now
+        host = self.configs.get('host') or FLASK_DEFAULT_HOST
+        port = self.configs.get('port') or FLASK_DEFAULT_PORT
+        debug = self.configs.get('debug') or FLASK_DEFAULT_DEBUG
+
+        print('Serving on http://{}:{}...'.format(host, port))
+
         app.run(
-            host=self.configs['host'],
-            port=self.configs['port'],
+            host=host,
+            port=port,
+            debug=debug,
         )
 
 plugin = Flask()
--- momo/plugins/flask/app.py
+++ momo/plugins/flask/app.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask
 
-FLASK_PLUGIN_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-FLASK_TEMPLATE_FOLDER = os.path.join(FLASK_PLUGIN_PATH, 'templates')
-FLASK_STATIC_FOLDER = os.path.join(FLASK_PLUGIN_PATH, 'static')
+FLASK_APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+FLASK_TEMPLATE_FOLDER = os.path.join(FLASK_APP_ROOT, 'templates')
+FLASK_STATIC_FOLDER = os.path.join(FLASK_APP_ROOT, 'static')
 
 app = Flask(
-    __name__,
+    import_name=__name__,
     template_folder=FLASK_TEMPLATE_FOLDER,
-    static_folder=FLASK_STATIC_FOLDER
+    static_folder=FLASK_STATIC_FOLDER,
 )
 
 
